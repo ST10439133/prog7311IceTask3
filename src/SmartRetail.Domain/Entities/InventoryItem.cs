0773ae35c188841ce2189252e80080c0a1d384fb
JGChetty17 <ST10296234@vcconnect.edu.za>
2026-05-06 23:57:33 +0200

    Comment changes

--- src/SmartRetail.Domain/Entities/InventoryItem.cs
+++ src/SmartRetail.Domain/Entities/InventoryItem.cs
@@ -5,5 +5,5 @@ public class InventoryItem
     public string ProductId { get; set; } = string.Empty;
     public int QuantityOnHand { get; set; }
     public int ReorderThreshold { get; set; }
-    public DateTime LastUpdated { get; set; }
+    public DateTime LastUpdated { get; set; }//DateTime
 }
